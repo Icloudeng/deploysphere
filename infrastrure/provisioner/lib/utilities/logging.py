5f9e73561aa46f73f7b839b2e79190a2ee0b7d01
Paradoxe Ngwasi <35149259+paradoxe35@users.noreply.github.com>
2023-06-26 12:37:53 +0200

    minor changes

--- infrastrure/provisioner/lib/utilities/logging.py
+++ infrastrure/provisioner/lib/utilities/logging.py
@@ -2,9 +2,14 @@ import logging
 
 
 def bingLoggingConfig(prefix: str = ''):
-    logging.basicConfig(
-        filename='python-lib.log',
-        encoding='utf-8',
-        level=logging.DEBUG,
-        format=f'{prefix}%(name)s - %(levelname)s - %(message)s'
-    )
+    root_logger = logging.getLogger()
+    root_logger.setLevel(logging.DEBUG)  # or whatever
+    handler = logging.FileHandler(
+        'python-lib.log', 'w', 'utf-8'
+    )  # or whatever
+    handler.setFormatter(
+        logging.Formatter(
+            f'{prefix}%(name)s - %(levelname)s - %(message)s'
+        )
+    )  # or whatever
+    root_logger.addHandler(handler)
