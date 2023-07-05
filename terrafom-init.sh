94eb481b69af3ff9991263fc84d1002f88bbf2ba
Paradoxe NGWASI <paradoxe.ngwasi@smatflow.com>
2023-07-05 16:57:17 +0000

    minor changes

--- terrafom-init.sh
+++ terrafom-init.sh
@@ -1,13 +1,16 @@
 set -u
 : "${GITLAB_ACCESS_TOKEN}"
 
+set -u
+: "${GITLAB_USERNAME}"
+
 statefile="platform-installer-dev"
 
 bin/terraform -chdir=infrastrure/terraform init \
     -backend-config="address=https://hub.smatflow.net/api/v4/projects/20/terraform/state/$statefile" \
     -backend-config="lock_address=https://hub.smatflow.net/api/v4/projects/20/terraform/state/$statefile/lock" \
     -backend-config="unlock_address=https://hub.smatflow.net/api/v4/projects/20/terraform/state/$statefile/lock" \
-    -backend-config="username=paradoxe.ngwasi" \
+    -backend-config="username=$GITLAB_USERNAME" \
     -backend-config="password=$GITLAB_ACCESS_TOKEN" \
     -backend-config="lock_method=POST" \
     -backend-config="unlock_method=DELETE" \
