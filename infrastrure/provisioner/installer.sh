6a0995addd2706c0f446ba121b65cd7ccbff096c
Paradoxe Ngwasi <35149259+paradoxe35@users.noreply.github.com>
2023-07-08 19:14:17 +0200

    minor changes

--- infrastrure/provisioner/installer.sh
+++ infrastrure/provisioner/installer.sh
@@ -83,7 +83,7 @@ ansible_extra_vars+=" random_secret=$random_secret admin_email=$admin_email" # M
 ansible_extra_vars+=" static_secret=$static_secret"                          # Must start with empty space
 
 # Notification Installer details
-installer_details="Platform: $platform\nMachine IP: $vm_ip\n"
+installer_details="Platform: $platform\nMachine IP: $vm_ip\n\n"
 installer_details+="Static Secret=$static_secret\n"
 
 # Notify before playbook
