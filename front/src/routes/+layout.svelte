94c6bea4f9a48f641dbc42cda2b101715c9267f2
Paradoxe NGWASI <paradoxe.ngwasi@smatflow.com>
2023-08-09 15:10:23 +0000

    front ui library setup

--- front/src/routes/+layout.svelte
+++ front/src/routes/+layout.svelte
@@ -1,9 +1,36 @@
 <script>
   import "./styles.css";
+  import {
+    fns,
+    AppShell,
+    Navbar,
+    Header,
+    Title,
+    Divider,
+  } from "@svelteuidev/core";
+
+  import {
+    colorScheme,
+    SvelteUIProvider,
+    Stack,
+    Switch,
+    Text,
+  } from "@svelteuidev/core";
+
+  function toggleTheme() {
+    colorScheme.update((v) => (v === "light" ? "dark" : "light"));
+  }
 </script>
 
-<div class="app">
-  <main>
+<SvelteUIProvider withGlobalStyles themeObserver={$colorScheme}>
+  <AppShell>
+    <!-- <Navbar slot="navbar" hidden={!opened} />
+  <Header slot="header" height="" /> -->
+    <Stack align="center">
+      <Text>Press to change the theme</Text>
+      <Switch on:change={toggleTheme} />
+    </Stack>
+
     <slot />
-  </main>
-</div>
+  </AppShell>
+</SvelteUIProvider>
