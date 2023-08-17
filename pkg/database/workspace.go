537c88a3e1ffd6dbd062d458d44cdf99c8d6274e
Paradoxe NGWASI <paradoxe.ngwasi@smatflow.com>
2023-08-17 15:15:37 +0000

    feat: workspace table

---
+++ pkg/database/workspace.go
@@ -0,0 +1,26 @@
+package database
+
+import "gorm.io/gorm"
+
+type Workspace struct {
+	gorm.Model
+	Name string `gorm:"index,unique"`
+}
+
+type WorkspaceRepository struct{}
+
+func (r *WorkspaceRepository) Get() []Workspace {
+	var objects []Workspace
+
+	dbConn.Find(&objects)
+
+	return objects
+}
+
+func (r *WorkspaceRepository) Create(res *Workspace) {
+	dbConn.Create(res)
+}
+
+func init() {
+	dbConn.AutoMigrate(&Workspace{})
+}
