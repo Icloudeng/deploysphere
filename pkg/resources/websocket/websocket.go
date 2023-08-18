3c0eb6006b4a11b68232ebaaf821acaf623c50fe
Paradoxe Ngwasi <35149259+paradoxe35@users.noreply.github.com>
2023-08-18 23:59:10 +0200

    tiny file structure changes

--- pkg/resources/websocket/websocket.go
+++ pkg/resources/websocket/websocket.go
@@ -1,10 +1,32 @@
 package websocket
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"smatflow/platform-installer/pkg/database"
+	"smatflow/platform-installer/pkg/http/ws"
 	"smatflow/platform-installer/pkg/pubsub"
 )
+
+func EmitDecodedEvent(playload pubsub.NetworkEventPayload) {
+	if json_data, err := json.Marshal(playload); err == nil {
+		ws.Broadcast(json_data)
+	}
+}
+
+func EmitEncodedEvent(playload pubsub.NetworkEventPayload) {
+	data := playload
+	decodedBytes, err := base64.StdEncoding.DecodeString(data.Payload)
+	if err != nil {
+		return
+	}
+
+	data.Payload = string(decodedBytes)
+
+	if json_data, err := json.Marshal(data); err == nil {
+		ws.Broadcast(json_data)
+	}
+}
 
 func EmitJobEvent(job *database.Job) {
 	playload := map[string]interface{}{
