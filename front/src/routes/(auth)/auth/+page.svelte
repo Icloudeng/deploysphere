04bad55d16da9b844804ec732c9df8e715342974
Paradoxe Ngwasi <35149259+paradoxe35@users.noreply.github.com>
2023-09-15 06:29:58 +0200

    front: tiny changes

--- front/src/routes/(auth)/auth/+page.svelte
+++ front/src/routes/(auth)/auth/+page.svelte
@@ -2,7 +2,14 @@
   import { Label, Input, Button } from "flowbite-svelte";
 </script>
 
-<form class="w-full md:w-1/2 flex justify-center flex-col mx-auto">
+<svelte:head>
+  <title>Login</title>
+</svelte:head>
+
+<form
+  class="w-full md:w-1/2 flex justify-center flex-col mx-auto"
+  autocomplete="off"
+>
   <div class="mb-6">
     <Label for="email" class="block mb-2">Email</Label>
     <Input id="email" type="email" placeholder="Email input" />
