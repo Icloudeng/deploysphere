aea047c7f1a7a5ac6054420dc33a45eed69c138f
Paradoxe Ngwasi <35149259+paradoxe35@users.noreply.github.com>
2023-09-15 06:12:30 +0200

    front: refactor layout

---
+++ front/src/routes/(app)/+layout.svelte
@@ -0,0 +1,68 @@
+<script>
+  import {
+    CompressOutline,
+    SortHorizontalSolid,
+    HourglassOutline,
+  } from "flowbite-svelte-icons";
+
+  import { page } from "$app/stores";
+  import Navbar from "../components/navbar.svelte";
+  import cn from "$lib/utils/cn";
+
+  $: activeUrl = $page.url.pathname;
+
+  $: menus = [
+    {
+      name: "Resources",
+      icon: CompressOutline,
+      href: "/",
+      active: activeUrl == "/",
+    },
+    {
+      name: "Jobs",
+      icon: SortHorizontalSolid,
+      href: "/jobs",
+      active: activeUrl == "/jobs",
+    },
+    {
+      name: "History",
+      icon: HourglassOutline,
+      href: "/history",
+      active: activeUrl == "/history",
+    },
+  ];
+</script>
+
+<svelte:head>
+  <title>Platform Installer</title>
+  <meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<div class="container mx-auto px-4 mt-3">
+  <Navbar />
+
+  <div class="border-b border-gray-200 dark:border-gray-700 mb-4">
+    <ul
+      class="flex flex-wrap -mb-px text-sm font-medium text-center text-gray-500 dark:text-gray-400"
+    >
+      {#each menus as menu, index (index)}
+        <li class="mr-2">
+          <a
+            href={menu.href}
+            class={cn(
+              "inline-flex items-center justify-center p-4",
+              menu.active &&
+                "border-b-2 border-b-primary-500 rounded-t-lg dark:text-white"
+            )}
+            aria-current="page"
+          >
+            <svelte:component this={menu.icon} class="mr-3 w-3 h-3" />
+            {menu.name}
+          </a>
+        </li>
+      {/each}
+    </ul>
+  </div>
+
+  <slot />
+</div>
---
+++ front/src/routes/(auth)/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { DarkMode } from "flowbite-svelte";
+</script>
+
+<div class="container mx-auto px-4 mt-3">
+  <DarkMode class="text-2xl" />
+
+  <div class="mt-4 w-full">
+    <slot />
+  </div>
+</div>
