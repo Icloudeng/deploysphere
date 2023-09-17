657df4115702e885fef54cb31b86a94886981a0c
Paradoxe Ngwasi <35149259+paradoxe35@users.noreply.github.com>
2023-09-17 12:20:09 +0200

    chore: client entity

--- pkg/database/client.go
+++ pkg/database/client.go
@@ -4,15 +4,15 @@ import "gorm.io/gorm"
 
 type Client struct {
 	gorm.Model
-	Country     string
+	Country     string `gorm:"uniqueIndex"`
 	CountryCode int
-	ClientEmail string
+	ClientEmail string `gorm:"uniqueIndex"`
 	ClientCode  int
 }
 
 type ClientRepository struct{}
 
-func (ClientRepository) Get(ID uint) *Client {
+func (ClientRepository) GetByID(ID uint) *Client {
 	object := &Client{}
 
 	dbConn.Last(object, ID)
@@ -24,8 +24,55 @@ func (ClientRepository) Get(ID uint) *Client {
 	return object
 }
 
-func (ClientRepository) Create(client *Client) {
-	dbConn.Create(client)
+func (ClientRepository) Get(country string, clientEmail string) *Client {
+	client := &Client{
+		Country:     country,
+		ClientEmail: clientEmail,
+	}
+
+	dbConn.Last(client)
+
+	return client
+}
+
+func (ClientRepository) Create(client Client) *Client {
+	client_email := Client{
+		Country:     client.Country,
+		ClientEmail: client.ClientEmail,
+	}
+	dbConn.Last(&client_email)
+
+	if client_email.ID != 0 {
+		return &client_email
+	}
+
+	client_country := Client{Country: client.Country}
+	dbConn.Last(&client_country)
+
+	if client_country.ID != 0 {
+		// Count client by country
+		var clients int64
+		dbConn.Where(&Client{
+			Country: client.Country,
+		}).Count(&clients)
+
+		client_country.ClientCode = int(clients + 1)
+		client_country.ClientEmail = client.ClientEmail
+
+		dbConn.Save(&client_country)
+		return &client_country
+	}
+
+	// Count countries
+	var countries int64
+	dbConn.Model(&Client{}).Select("country, count(ID) as total_country").Group("name").Count(&countries)
+
+	client.CountryCode = int(countries + 1)
+	client.ClientCode = 1
+
+	dbConn.Save(&client)
+
+	return &client
 }
 
 func init() {
---
+++ pkg/http/handlers/client.go
@@ -0,0 +1,52 @@
+package handlers
+
+import (
+	"net/http"
+	"smatflow/platform-installer/pkg/database"
+
+	"github.com/gin-gonic/gin"
+)
+
+type (
+	clientBody struct {
+		Country     string `json:"country" binding:"required,string"`
+		ClientEmail string `json:"client_email" binding:"required,string,email"`
+	}
+
+	clientHandler struct{}
+)
+
+var Client clientHandler
+
+func (resourcesHandler) CreateClient(c *gin.Context) {
+	var json clientBody
+
+	if err := c.ShouldBindJSON(&json); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	repository := database.ClientRepository{}
+
+	client := repository.Create(database.Client{
+		Country:     json.Country,
+		ClientEmail: json.ClientEmail,
+	})
+
+	c.JSON(http.StatusOK, client)
+}
+
+func (resourcesHandler) GetClient(c *gin.Context) {
+	var json clientBody
+
+	if err := c.ShouldBindJSON(&json); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	repository := database.ClientRepository{}
+
+	client := repository.Get(json.Country, json.ClientEmail)
+
+	c.JSON(http.StatusOK, client)
+}
