f056553f66d7dcf201f73272999dd13b82899d30
Paradoxe Ngwasi <35149259+paradoxe35@users.noreply.github.com>
2023-09-28 00:08:01 +0200

    minor improvements

--- pkg/http/handlers/resources_state.go
+++ pkg/http/handlers/resources_state.go
@@ -3,7 +3,6 @@ package handlers
 import (
 	"net/http"
 	"smatflow/platform-installer/pkg/database/entities"
-	"smatflow/platform-installer/pkg/resources/terraform"
 
 	"github.com/gin-gonic/gin"
 )
@@ -25,23 +24,9 @@ func (resourcesStateHandler) GetByRef(c *gin.Context) {
 
 	// Resource state from db
 	repository := entities.ResourcesStateRepository{}
-	db_resources := repository.GetByRef(uri.Ref)
-
-	// Get resource state from terraform proxmox module
-	proxmox := terraform.ResourceState{
-		Module: "proxmox",
-	}
-	vm_qemu := proxmox.GetResourceState(uri.Ref)
-
-	// Get resource state from terraform proxmox OVH
-	ovh := terraform.ResourceState{
-		Module: "ovh",
-	}
-	domain_zone_record := ovh.GetResourceState(uri.Ref)
+	resources := repository.GetByRef(uri.Ref)
 
 	c.JSON(http.StatusOK, gin.H{
-		"database":               db_resources,
-		"proxmox_vm_qemu":        vm_qemu,
-		"ovh_domain_zone_record": domain_zone_record,
+		"data": resources,
 	})
 }
