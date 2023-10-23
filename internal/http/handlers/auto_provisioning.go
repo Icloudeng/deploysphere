4c5beae37baf47fcfad1973ec5b05075440354bd
Paradoxe Ngwasi <paradoxngwasi@gmail.com>
2023-10-23 06:19:25 +0000

    tiny changes

--- internal/http/handlers/auto_provisioning.go
+++ internal/http/handlers/auto_provisioning.go
@@ -81,6 +81,15 @@ func (provisioningHandler) CreateAutoConfigurationProvisioning(ctx *gin.Context)
 		return
 	}
 
+	ctx.JSON(http.StatusOK, gin.H{"data": body, "job": structs.AutoConfiguration{
+		Type:              body.Type,
+		Platform:          platformName,
+		PlatformRef:       platformRef,
+		PlatformConfigRef: platformConfigRef,
+	}})
+
+	return
+
 	output, err := provisioning.CreateAutoConfigurationProvisioning(structs.AutoConfiguration{
 		Type:              body.Type,
 		Platform:          platformName,
