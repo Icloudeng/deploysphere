96a2905d8bf944fd8ece7d5546bd83d6a385677e
Paradoxe Ngwasi <paradoxngwasi@gmail.com>
2023-11-07 17:12:24 +0000

    update authentik ldap federation

--- infrastructure/provisioner/scripts/platforms/authentik/configuration/scripts/authentik-ldap-federation.py
+++ infrastructure/provisioner/scripts/platforms/authentik/configuration/scripts/authentik-ldap-federation.py
@@ -17,7 +17,15 @@ LDAP_BIND_DN = os.getenv("LDAP_BIND_DN")
 LDAP_BIND_PASSWORD = os.getenv("LDAP_BIND_PASSWORD")
 
 
-for value in (SESSION_HEADERS, AUTHENTIK_URL, PROVIDER_NAME, LDAP_SERVER_URL, LDAP_BASE_CN, LDAP_BIND_DN, LDAP_BIND_PASSWORD):
+for value in (
+    SESSION_HEADERS,
+    AUTHENTIK_URL,
+    PROVIDER_NAME,
+    LDAP_SERVER_URL,
+    LDAP_BASE_CN,
+    LDAP_BIND_DN,
+    LDAP_BIND_PASSWORD,
+):
     if not value or len(value) == 0:
         raise Exception("Envs varaible required !")
 
@@ -27,14 +35,14 @@ headers = {}
 
 
 def slugify(text):
-    text = re.sub(r'[^\w\s-]', '', text).strip().lower()
-    text = re.sub(r'[-\s]+', '-', text)
+    text = re.sub(r"[^\w\s-]", "", text).strip().lower()
+    text = re.sub(r"[-\s]+", "-", text)
     return text
 
 
 def generate_random_key(length: int):
     characters = string.ascii_letters + string.digits
-    random_key = ''.join(secrets.choice(characters) for _ in range(length))
+    random_key = "".join(secrets.choice(characters) for _ in range(length))
     return random_key
 
 
@@ -55,7 +63,7 @@ def get_pk(results=[], field=None, value=None):
 def get_ldap_property_mappings():
     response = requests.get(
         f"{AUTHENTIK_URL}/propertymappings/ldap/?ordering=managed%2Cobject_field",
-        headers=headers
+        headers=headers,
     )
 
     response.raise_for_status()
@@ -64,10 +72,8 @@ def get_ldap_property_mappings():
 
 
 def get_source_ldap(name: str):
-    name = slugify(name)
     response = requests.get(
-        f"{AUTHENTIK_URL}/sources/all/?search={name}",
-        headers=headers
+        f"{AUTHENTIK_URL}/sources/all/?search={name}", headers=headers
     )
 
     response.raise_for_status()
@@ -76,61 +82,63 @@ def get_source_ldap(name: str):
 
 
 def main():
+    provider_name_slugified = slugify(PROVIDER_NAME)
     provider_pk = get_pk(
-        get_source_ldap(PROVIDER_NAME),
-        "name",
-        PROVIDER_NAME
+        get_source_ldap(provider_name_slugified), "name", PROVIDER_NAME
     )
 
-    if provider_pk:
-        return
-
     mappings = get_ldap_property_mappings()
+    body = {
+        "name": PROVIDER_NAME,
+        "slug": provider_name_slugified,
+        "enabled": True,
+        "user_path_template": "goauthentik.io/sources/%(slug)s",
+        "server_uri": LDAP_SERVER_URL,
+        "peer_certificate": "",
+        "client_certificate": "",
+        "bind_cn": LDAP_BIND_DN,
+        "bind_password": LDAP_BIND_PASSWORD,
+        "start_tls": False,
+        "sni": False,
+        "base_dn": LDAP_BASE_CN,
+        "additional_user_dn": "cn=users",
+        "additional_group_dn": "cn=groups",
+        "user_object_filter": "(objectClass=person)",
+        "group_object_filter": "(objectClass=nestedGroup)",
+        "group_membership_field": "member",
+        "object_uniqueness_field": "ipaUniqueID",
+        "sync_users": True,
+        "sync_users_password": True,
+        "sync_groups": True,
+        "sync_parent_group": "",
+        "property_mappings": get_pk(mappings),
+        "property_mappings_group": [
+            get_pk(mappings, "managed", "goauthentik.io/sources/ldap/default-name"),
+            get_pk(mappings, "managed", "goauthentik.io/sources/ldap/openldap-cn"),
+        ],
+    }
 
-    requests.post(
-        f"{AUTHENTIK_URL}/sources/ldap/",
-        headers=headers,
-        json={
-            "name": PROVIDER_NAME,
-            "slug": slugify(PROVIDER_NAME),
-            "enabled": True,
-            "user_path_template": "goauthentik.io/sources/%(slug)s",
-            "server_uri": LDAP_SERVER_URL,
-            "peer_certificate": "",
-            "client_certificate": "",
-            "bind_cn": LDAP_BIND_DN,
-            "bind_password": LDAP_BIND_PASSWORD,
-            "start_tls": False,
-            "sni": False,
-            "base_dn": LDAP_BASE_CN,
-            "additional_user_dn": "cn=users",
-            "additional_group_dn": "cn=groups",
-            "user_object_filter": "(objectClass=person)",
-            "group_object_filter": "(objectClass=nestedGroup)",
-            "group_membership_field": "member",
-            "object_uniqueness_field": "ipaUniqueID",
-            "sync_users": True,
-            "sync_users_password": True,
-            "sync_groups": True,
-            "sync_parent_group": "",
-            "property_mappings": get_pk(mappings),
-            "property_mappings_group": [
-                get_pk(
-                    mappings,
-                    "managed",
-                    "goauthentik.io/sources/ldap/default-name"
-                ),
-                get_pk(
-                    mappings,
-                    "managed",
-                    "goauthentik.io/sources/ldap/openldap-cn"
-                ),
-            ]
-        }
-    ).raise_for_status()
+    if provider_pk:
+        requests.patch(
+            f"{AUTHENTIK_URL}/sources/ldap/{provider_name_slugified}/",
+            headers=headers,
+            json=body,
+        ).raise_for_status()
+    else:
+        requests.post(
+            f"{AUTHENTIK_URL}/sources/ldap/",
+            headers=headers,
+            json=body,
+        ).raise_for_status()
+        # Send another request to synchonize LDAP entries
+        requests.patch(
+            f"{AUTHENTIK_URL}/sources/ldap/{provider_name_slugified}/",
+            headers=headers,
+            json=body,
+        ).raise_for_status()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     decoded_bytes = base64.b64decode(SESSION_HEADERS)
     headers = json.loads(decoded_bytes.decode("utf-8"))
     main()
