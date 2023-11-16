b662d30309e272d5342738f7277d19ec8a7dbc59
Paradoxe Ngwasi <paradoxngwasi@gmail.com>
2023-11-16 07:12:48 +0000

    Update DomainZoneRecord validation in domain.go
    - Change the binding tag for the Subdomain field to
    "omitempty,ascii,lowercase"
    - Add more options to the Fieldtype binding tag

--- internal/structs/domain.go
+++ internal/structs/domain.go
@@ -2,7 +2,7 @@ package structs
 
 type DomainZoneRecord struct {
 	Zone      string `json:"zone" binding:"required"`
-	Subdomain string `json:"subdomain" binding:"required,ascii,lowercase"`
+	Subdomain string `json:"subdomain" binding:"omitempty,ascii,lowercase"`
 	Fieldtype string `json:"fieldtype" binding:"required,oneof=A AAAA CNAME DNAME NS MX SPF DKIM DMARC TXT SRV CAA NAPTR LOC SSHFP TLSA"`
 	Ttl       int    `json:"ttl" binding:"required,number,gte=60"`
 	Target    string `json:"target" binding:"required"`
