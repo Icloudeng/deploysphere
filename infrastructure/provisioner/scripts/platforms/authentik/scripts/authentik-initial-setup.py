e037f7d470da7f28e674cce77bb4f76884184609
Paradoxe Ngwasi <35149259+paradoxe35@users.noreply.github.com>
2023-11-26 18:50:06 +0200

    Refactor Authentik initial setup process and introduce Python script

---
+++ infrastructure/provisioner/scripts/platforms/authentik/scripts/authentik-initial-setup.py
@@ -0,0 +1,44 @@
+import os
+import json
+import base64
+import requests
+
+SESSION_HEADERS = os.getenv("SESSION_HEADERS")
+AUTHENTIK_URL = os.getenv("AUTHENTIK_URL")
+AUTHENTIK_ADMIN_EMAIL = os.getenv("AUTHENTIK_ADMIN_EMAIL")
+AUTHENTIK_ADMIN_PASSWORD = os.getenv("AUTHENTIK_ADMIN_PASSWORD")
+
+
+for value in (
+    SESSION_HEADERS,
+    AUTHENTIK_URL,
+    AUTHENTIK_ADMIN_EMAIL,
+    AUTHENTIK_ADMIN_PASSWORD,
+):
+    if not value or len(value) == 0:
+        raise Exception("Envs varaible required !")
+
+AUTHENTIK_URL = f"{AUTHENTIK_URL}/api/v3"
+
+headers = {}
+
+
+def main():
+    body = {
+        "email": AUTHENTIK_ADMIN_EMAIL,
+        "password": AUTHENTIK_ADMIN_PASSWORD,
+        "password_repeat": AUTHENTIK_ADMIN_PASSWORD,
+        "component": "ak-stage-prompt",
+    }
+
+    requests.post(
+        f"{AUTHENTIK_URL}/flows/executor/initial-setup/?query=",
+        headers=headers,
+        json=body,
+    ).raise_for_status()
+
+
+if __name__ == "__main__":
+    decoded_bytes = base64.b64decode(SESSION_HEADERS)
+    headers = json.loads(decoded_bytes.decode("utf-8"))
+    main()
