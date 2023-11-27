a6953578e3c685e0f27640738a3cb313bfb88594
Paradoxe Ngwasi <paradoxngwasi@gmail.com>
2023-11-27 04:58:35 +0000

    feat: Add LDAP configuration for Penpot provisioning

---
+++ infrastructure/provisioner/scripts/configurations/penpot/ldap.py
@@ -0,0 +1,54 @@
+import sys
+import os
+
+# Get the parent directory
+parent_dir = os.getcwd()
+# # Add the parent directory to sys.path
+sys.path.append(parent_dir)
+
+from lib.utilities.auto_configuration import (
+    get_resources_state,
+    post_provisioning_configuration,
+    domain_to_ldap_dc,
+    get_command_args,
+    log,
+)
+
+
+def main(args):
+    # penpot
+    penpot_reference = args.reference
+
+    # FreeIPA
+    freeipa_state = get_resources_state(args.config_reference)["data"]
+    ipa_domain = freeipa_state["Job"]["PostBody"]["platform"]["metadata"]["ipa_domain"]
+    ipa_domain_dc = domain_to_ldap_dc(ipa_domain)
+    freeipa_credentials = freeipa_state["Credentials"][0]
+    freeipa_ipv4_address = freeipa_state["State"]["proxmox_vm_qemu"]["values"][
+        "default_ipv4_address"
+    ]
+
+    body = {
+        "ref": penpot_reference,
+        "platform": {
+            "name": args.platform,
+            "metadata": {
+                "configuration_type": args.type,
+                "configuration": {
+                    "ldap_server_host": freeipa_ipv4_address,
+                    "ldap_server_port": "389",
+                    "ldap_search_base": "cn=accounts,%s" % ipa_domain_dc,
+                    "ldap_bind_dn": "uid=admin,cn=users,cn=accounts,%s" % ipa_domain_dc,
+                    "ldap_bind_password": freeipa_credentials["password"],
+                },
+            },
+        },
+    }
+
+    response = post_provisioning_configuration(body)
+    log(response["job"]["ID"])
+
+
+if __name__ == "__main__":
+    args = get_command_args()
+    main(args)
