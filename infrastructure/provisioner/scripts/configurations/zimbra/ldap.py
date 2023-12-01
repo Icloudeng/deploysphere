15bc450945f0b55c14ac4029f57b3e00ac96dfb2
Paradoxe Ngwasi <paradoxngwasi@gmail.com>
2023-12-01 07:39:58 +0000

    feat: zimbra ldap auto configuration

---
+++ infrastructure/provisioner/scripts/configurations/zimbra/ldap.py
@@ -0,0 +1,60 @@
+import sys
+import os
+
+# Get the parent directory
+parent_dir = os.getcwd()
+# # Add the parent directory to sys.path
+sys.path.append(parent_dir)
+
+from lib.utilities.auto_configuration import (
+    get_resources_state,
+    post_provisioning_configuration,
+    domain_to_ldap_dc,
+    get_command_args,
+    log,
+    extract_subdomain,
+)
+
+
+def main(args):
+    # zimbra
+    zimbra_reference = args.reference
+
+    # FreeIPA
+    freeipa_state = get_resources_state(args.config_reference)["data"]
+    metadata = freeipa_state["Job"]["PostBody"]["platform"]["metadata"]
+    ipa_domain = metadata.get("ipa_domain")
+
+    if not ipa_domain:
+        ipa_domain = extract_subdomain(metadata.get("domain"))
+
+    ipa_domain_dc = domain_to_ldap_dc(ipa_domain)
+    freeipa_credentials = freeipa_state["Credentials"][0]
+    freeipa_ipv4_address = freeipa_state["State"]["proxmox_vm_qemu"]["values"][
+        "default_ipv4_address"
+    ]
+
+    body = {
+        "ref": zimbra_reference,
+        "platform": {
+            "name": args.platform,
+            "metadata": {
+                "configuration_type": args.type,
+                "configuration": {
+                    "ldap_filter_username": "uid=%u",
+                    "ldap_server_url": "ldap://%s:389" % freeipa_ipv4_address,
+                    "ldap_search_base": "cn=accounts,%s" % ipa_domain_dc,
+                    "ldap_bind_dn": "uid=admin,cn=users,cn=accounts,%s" % ipa_domain_dc,
+                    "ldap_bind_password": freeipa_credentials["password"],
+                },
+            },
+        },
+    }
+
+    response = post_provisioning_configuration(body)
+    log(response["job"]["ID"])
+
+
+if __name__ == "__main__":
+    args = get_command_args()
+    main(args)
