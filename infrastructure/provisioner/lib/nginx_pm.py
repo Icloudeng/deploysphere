ec443ab47251866eb61b44c942d4b8e6052c984e
Paradoxe Ngwasi <paradoxngwasi@gmail.com>
2024-01-23 10:13:05 +0000

    improve nginx proxymanager

--- infrastructure/provisioner/lib/nginx_pm.py
+++ infrastructure/provisioner/lib/nginx_pm.py
@@ -7,10 +7,7 @@ from utilities.dotenv import config
 from utilities.logging import logging, bingLoggingConfig
 
 
-headers = {
-    "Content-Type": "application/json",
-    "Accept": "application/json"
-}
+headers = {"Content-Type": "application/json", "Accept": "application/json"}
 
 DOMAIN_KEY = "domain"
 SSL_PROVIDER = "letsencrypt"
@@ -18,14 +15,19 @@ SSL_PROVIDER = "letsencrypt"
 
 def clean_domain(url: str):
     if url.startswith("http://"):
-        url = url[len("http://"):]
+        url = url[len("http://") :]
     elif url.startswith("https://"):
-        url = url[len("https://"):]
+        url = url[len("https://") :]
 
     if url.endswith("/"):
         url = url[:-1]
 
     return url
+
+
+def remove_none_values(input_list):
+    # Filter out all None values from the input_list
+    return [item for item in input_list if item is not None]
 
 
 def get_api_token():
@@ -42,17 +44,15 @@ def get_api_token():
     res = requests.post(
         f"{url}/api/tokens",
         json={"identity": email, "secret": password},
-        headers=headers
+        headers=headers,
     )
     if res.status_code != 200:
-        logging.warning(
-            "Nginx proxy mananger authentication failed"
-        )
+        logging.warning("Nginx Proxy Manager authentication failed")
         return None, None
 
     json = res.json()
 
-    token = json.get('token')
+    token = json.get("token")
 
     headers["Authorization"] = f"Bearer {token}"
 
@@ -61,31 +61,39 @@ def get_api_token():
 
 def get_decoded_domain(metadata: str):
     decoded_bytes = base64.b64decode(metadata)
-    data = json.loads(decoded_bytes.decode("utf-8"))
-    domain = data.get(DOMAIN_KEY, None)
-    # If _mapping was passed then ignore the current _mapping processs
-    ignore = data.get("_mapping", None) == False or data.get(
-        "_mapping", None) == "false"
+    data: dict[str, str] = json.loads(decoded_bytes.decode("utf-8"))
+    domain_main = data.get(DOMAIN_KEY, None)
+    # If _mapping was passed then ignore the current _mapping process
+    ignore = (
+        data.get("_mapping", None) == False or data.get("_mapping", None) == "false"
+    )
 
     if ignore:
         return None
 
-    return clean_domain(domain) if domain else None
+    domains = [clean_domain(domain_main) if domain_main else None]
+
+    for key in data.keys():
+        if key.endswith("_domain") and data.get(key):
+            domains.append(
+                clean_domain(data.get(key)),
+            )
+
+    domains = remove_none_values(domains)
+
+    return domains if len(domains) > 0 else None
 
 
-def delete_proxy_hosts(phost: Any, url: str):
-    if phost:
+def delete_proxy_hosts(pHost: Any, url: str):
+    if pHost:
         requests.delete(
-            f"{url}/api/nginx/proxy-hosts/{phost.get('id')}",
-            headers=headers
+            f"{url}/api/nginx/proxy-hosts/{pHost.get('id')}", headers=headers
         )
 
 
-def find_existing_proxy_host(domain: str, url: str):
-    res = requests.get(
-        f"{url}/api/nginx/proxy-hosts?query={domain}",
-        headers=headers
-    )
+def find_existing_proxy_host(domains: list[str], url: str):
+    fDomain = domains[0]
+    res = requests.get(f"{url}/api/nginx/proxy-hosts?query={fDomain}", headers=headers)
     if res.status_code != 200:
         return None
     # Check for the API Schema
@@ -93,23 +101,28 @@ def find_existing_proxy_host(domain: str, url: str):
 
     data: List[Any] = res.json()
 
-    for phost in data:
-        domains: List[str] = phost.get('domain_names')
-        try:
-            domains.index(domain)
-            return phost
-        except Exception as err:
-            logging.error(err)
-            continue
+    for pHost in data:
+        validHost = True
+
+        domain_names: List[str] = pHost.get("domain_names")
+
+        for domain in domains:
+            try:
+                domain_names.index(domain)
+            except Exception as err:
+                validHost = False
+                logging.error(err)
+                continue
+
+        if validHost:
+            return pHost
 
     return None
 
 
-def find_existing_certificate(domain: str, url: str):
-    res = requests.get(
-        f"{url}/api/nginx/certificates?query={domain}",
-        headers=headers
-    )
+def find_existing_certificate(domains: list[str], url: str):
+    fDomain = domains[0]
+    res = requests.get(f"{url}/api/nginx/certificates?query={fDomain}", headers=headers)
     if res.status_code != 200:
         return None
     # Check for the API Schema
@@ -117,13 +130,21 @@ def find_existing_certificate(domain: str, url: str):
 
     data: List[Any] = res.json()
 
-    for phost in data:
-        domains: List[str] = phost['domain_names']
-        try:
-            domains.index(domain)
-            return phost
-        except:
-            continue
+    for pCert in data:
+        validCert = True
+
+        domain_names: List[str] = pCert.get("domain_names")
+
+        for domain in domains:
+            try:
+                domain_names.index(domain)
+            except Exception as err:
+                validCert = False
+                logging.error(err)
+                continue
+
+        if validCert:
+            return pCert
 
     return None
 
@@ -144,27 +165,23 @@ def get_platform_nginx_pm_config(platform: str):
     return data.get(platform)
 
 
-def create_domain_certificate(domain: str, url: str):
-    certificate = find_existing_certificate(domain, url)
+def create_domains_certificate(domains: List[str], url: str):
+    certificate = find_existing_certificate(domains, url)
     if certificate:
         return certificate
 
     body = {
-        "domain_names": [domain],
+        "domain_names": domains,
         "meta": {
             "letsencrypt_email": config.get("ADMIN_SYSTEM_EMAIL", "admin@example.com"),
             "letsencrypt_agree": True,
-            "dns_challenge": False
+            "dns_challenge": False,
         },
-        "provider": SSL_PROVIDER
+        "provider": SSL_PROVIDER,
     }
     # Check for the API Schema
     # (https://github.com/NginxProxyManager/nginx-proxy-manager/blob/develop/backend/schema/endpoints/certificates.json)
-    res = requests.post(
-        f"{url}/api/nginx/certificates",
-        json=body,
-        headers=headers
-    )
+    res = requests.post(f"{url}/api/nginx/certificates", json=body, headers=headers)
 
     if res.status_code >= 200 and res.status_code < 400:
         return res.json()
@@ -172,100 +189,92 @@ def create_domain_certificate(domain: str, url: str):
     return None
 
 
-def create_proxy_host(url: str, domain: str, certificate: Any, platform_protocol: Any, ip: str, platform: str):
+def create_proxy_host(
+    url: str,
+    domains: List[str],
+    certificate: Any,
+    platform_protocol: Any,
+    ip: str,
+    platform: str,
+):
     certificate = certificate if certificate else {}
-    certificate_id = certificate.get('id')
+    certificate_id = certificate.get("id")
 
     advanced_config = get_platform_nginx_pm_config(platform)
-    advanced_config = (
-        "" if not advanced_config else advanced_config
-    ).replace("\\n", "\n")
+    advanced_config = ("" if not advanced_config else advanced_config).replace(
+        "\\n", "\n"
+    )
 
     body = {
-        "domain_names": [domain],
+        "domain_names": domains,
         "forward_host": ip,
-        "forward_scheme": platform_protocol.get('protocol'),
-        "forward_port": platform_protocol.get('port'),
+        "forward_scheme": platform_protocol.get("protocol"),
+        "forward_port": platform_protocol.get("port"),
         "block_exploits": True,
         "allow_websocket_upgrade": True,
         "access_list_id": "0",
         "certificate_id": certificate_id if certificate_id else 0,
         "ssl_forced": True if certificate_id else False,
-        "http2_support":  True if certificate_id else False,
-        "hsts_enabled":  True if certificate_id else False,
-        "meta": {
-            "letsencrypt_agree": False,
-            "dns_challenge": False
-        },
+        "http2_support": True if certificate_id else False,
+        "hsts_enabled": True if certificate_id else False,
+        "meta": {"letsencrypt_agree": False, "dns_challenge": False},
         "advanced_config": advanced_config,
         "locations": [],
         "caching_enabled": False,
-        "hsts_subdomains": False
+        "hsts_subdomains": False,
     }
 
-    requests.post(
-        f"{url}/api/nginx/proxy-hosts",
-        json=body,
-        headers=headers
-    )
+    requests.post(f"{url}/api/nginx/proxy-hosts", json=body, headers=headers)
 
 
 def main(action: str, metadata: str, platform: str, ip: str):
-    # Decode metade and get the domain value
-    domain = get_decoded_domain(metadata)
-    if not domain:
+    # Decode metadata and get the domain value
+    domains = get_decoded_domain(metadata)
+    if not domains:
         return
 
     url, token = get_api_token()
     if not url or not token:
         return
 
-    phost = find_existing_proxy_host(domain, url)
+    pHost = find_existing_proxy_host(domains, url)
 
     # Check of delete action
     if action == "delete":
-        logging.info(
-            "Deleting... proxy host"
-        )
-        delete_proxy_hosts(phost, url)
+        logging.info("Deleting... proxy host")
+        delete_proxy_hosts(pHost, url)
         return
 
-    # If phost exists and delete it
-    if phost:
-        delete_proxy_hosts(phost, url)
+    # If pHost exists and delete it
+    if pHost:
+        delete_proxy_hosts(pHost, url)
 
     # Get platform proxy
     platform_protocol = get_platform_protocol(platform)
 
     if not platform_protocol:
-        logging.info(
-            "Cannot found the corresponding platform protocol"
-        )
+        logging.info("Cannot found the corresponding platform protocol")
         return
 
     # Generate domain certificate
-    certificate = create_domain_certificate(domain, url)
+    certificate = create_domains_certificate(domains, url)
 
     # Finally create the proxy host
     create_proxy_host(
         url=url,
-        domain=domain,
+        domains=domains,
         certificate=certificate,
         platform_protocol=platform_protocol,
         ip=ip,
-        platform=platform
+        platform=platform,
     )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     bingLoggingConfig(prefix="NGINX PM / ")
 
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--action",
-        choices=['create', 'delete'],
-        required=True
-    )
+    parser.add_argument("--action", choices=["create", "delete"], required=True)
     parser.add_argument("--metadata", required=True)
     parser.add_argument("--platform", required=False)
     parser.add_argument("--ip", required=False)
